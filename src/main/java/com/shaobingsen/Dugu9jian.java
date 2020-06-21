7fc0bb121b824b7eadbbfcadb7a5c70400153f78
shaobingsen <sen123163@163.com>
2020-06-21 22:21:54 +0800

    update 1.1

--- src/main/java/com/shaobingsen/Dugu9jian.java
+++ src/main/java/com/shaobingsen/Dugu9jian.java
@@ -3,5 +3,9 @@ package com.shaobingsen;
 public class Dugu9jian {
     public static void main(String[] args) {
         System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
     }
 }
---
+++ src/main/java/com/shaobingsen/Huashan.java
@@ -0,0 +1,7 @@
+package com.shaobingsen;
+
+public class Huashan {
+    public static void main(String[] args) {
+        System.out.println("huahsan");
+    }
+}
